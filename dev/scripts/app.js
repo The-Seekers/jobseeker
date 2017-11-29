dd2d00bc22481e698745486e4f2761f94bdac618
Sam Janosik <samanthajanosik@gmail.com>
2017-11-29 12:38:32 -0500

    single application view with edit button enabled

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -5,13 +5,26 @@ import {
   BrowserRouter as Router,
   Route, Link
 } from 'react-router-dom';
+import ApplicationList from './jobApplication'
+
+var config = {
+  apiKey: "AIzaSyA1qK4MCT9BNUPRKbz6Wy1OeKEFLYswWW8",
+  authDomain: "job-seekers-16fc9.firebaseapp.com",
+  databaseURL: "https://job-seekers-16fc9.firebaseio.com",
+  projectId: "job-seekers-16fc9",
+  storageBucket: "job-seekers-16fc9.appspot.com",
+  messagingSenderId: "658056882007"
+};
+firebase.initializeApp(config);
 
 class App extends React.Component {
     render() {
       return (
-        <div>
-          JOBSEEKERS
-        </div>
+          <div>
+            SEEKERS
+            <ApplicationList />
+            
+          </div>
       )
     }
 }
