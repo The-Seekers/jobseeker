dce802ea8641088601aef53cc291a2e2c61860d2
Dylan On <dylanon@users.noreply.github.com>
2017-11-29 12:19:44 -0500

    Dynamically set what happens when the form is submitted

--- dev/scripts/authForm.js
+++ dev/scripts/authForm.js
@@ -2,25 +2,40 @@ import React from 'react';
 import firebase from 'firebase';
 
 class AuthForm extends React.Component {
+    signUp(e) {
+        e.preventDefault();
+        console.log('signing up');
+    }
+
+    signIn(e) {
+        e.preventDefault();
+        console.log('signing in');
+    }
+
     render() {
         let title = '';
         let buttonText = '';
         let passwordText = '';
+        let formAction = () => null;
 
         if (this.props.formToDisplay === 'signUp') {
             title = 'Sign Up';
             buttonText = 'Sign Up';
             passwordText = 'Choose a Password';
+            formAction = this.signUp;
         } else if (this.props.formToDisplay === 'signIn') {
             title = 'Sign In';
             buttonText = 'Sign In';
             passwordText = 'Password';
+            formAction = this.signIn;
         }
+
+        console.log(formAction);
 
         return (
             <div>
                 <h2>{title}</h2>
-                <form action="">
+                <form action="" onSubmit={(e) => {formAction(e)}}>
                     <div>
                         <label htmlFor="auth-email">E-mail</label>
                         <input id="auth-email" type="email" required/>
