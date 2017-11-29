8268e680897e92ba62038cfbbf2f6b9e62e51080
Dylan On <dylanon@users.noreply.github.com>
2017-11-29 10:34:24 -0500

    Add firebase config

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -6,6 +6,17 @@ import {
   Route, Link
 } from 'react-router-dom';
 
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyA1qK4MCT9BNUPRKbz6Wy1OeKEFLYswWW8",
+  authDomain: "job-seekers-16fc9.firebaseapp.com",
+  databaseURL: "https://job-seekers-16fc9.firebaseio.com",
+  projectId: "job-seekers-16fc9",
+  storageBucket: "job-seekers-16fc9.appspot.com",
+  messagingSenderId: "658056882007"
+};
+firebase.initializeApp(config);
+
 class App extends React.Component {
     render() {
       return (
