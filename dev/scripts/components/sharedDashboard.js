5631bdd8c3001e542bf9d15a96f08e0328301d52
Dylan On <dylanon@users.noreply.github.com>
2017-11-30 22:50:28 -0500

    Only fetch and display job data if the user has turned on sharing

--- dev/scripts/components/sharedDashboard.js
+++ dev/scripts/components/sharedDashboard.js
@@ -6,32 +6,70 @@ export default class SharedDashboard extends React.Component {
     constructor() {
         super();
         this.state = {
-            applications: []
+            applications: [],
+            sharingEnabled: false
         }
     }
 
-    // Pull the applications from Firebase, store in state
     componentDidMount() {
-        const applicationsRef = firebase.database().ref(`users/${this.props.match.params.userId}/applications`);
-        applicationsRef.on('value', (snapshot) => {
-            const applicationsArray = [];
-            const applicationItems = snapshot.val();
-            for (let applicationKey in applicationItems) {
-                applicationItems[applicationKey].key = applicationKey;
-                applicationsArray.push(applicationItems[applicationKey]);
+        // Set a database reference to the supplied sharing key
+        const sharingRef = firebase.database().ref(`users/${this.props.match.params.userId}/sharing/${this.props.match.params.shareKey}`);
+        
+        // Check if the user has enabled sharing
+        sharingRef.once('value', (snapshot) => {
+            const shareKeyValue = snapshot.val();
+            if (shareKeyValue) {
+                this.setState({
+                    sharingEnabled: true
+                });
             }
-            this.setState({
-                applications: applicationsArray
-            });
+        })
+        .then(() => {
+            if (this.state.sharingEnabled) {
+                console.log('sharing is enabled - loading the job applications');
+                // Pull the applications from Firebase, store in state
+                const applicationsRef = firebase.database().ref(`users/${this.props.match.params.userId}/applications`);
+                applicationsRef.on('value', (snapshot) => {
+                    const applicationsArray = [];
+                    const applicationItems = snapshot.val();
+                    for (let applicationKey in applicationItems) {
+                        applicationItems[applicationKey].key = applicationKey;
+                        applicationsArray.push(applicationItems[applicationKey]);
+                    }
+                    this.setState({
+                        applications: applicationsArray
+                    });
+                });
+            } else {
+                console.log('This user has not enabled sharing.');
+            }
         });
+        
     }
 
     render() {
+        // Build the display markup
+        let display = '';
+        if (this.state.sharingEnabled) {
+            display = (
+                <div>
+                    <DashStats />
+                    <ApplicationList applications={this.state.applications} />
+                </div>
+            )
+        } else {
+            display = (
+                <div>
+                    <h2>Oops...</h2>
+                    <p>This user has not enabled sharing for their applications. Ask them to turn sharing on!</p>
+                </div>
+            )
+        }
+
+        // Display the markup
         return (
             <main>
-                {/* <DashWelcome /> */}
-                <DashStats />
-                <ApplicationList applications={this.state.applications} />
+                {display}
             </main>
         )
     }
@@ -48,24 +86,6 @@ class DashStats extends React.Component {
                     <li>temp statcard</li>
                 </ul>
             </section>
-        )
-    }
-}
-
-// search, sort, navigation for applicationList
-class JobsListNav extends React.Component {
-    render() {
-        return (
-            <nav>
-                <form>
-                    <select name='sortBy'>
-                        <option value='sortOption'>sort by...</option>
-                        <option value='sortOption'>sort by...</option>
-                        <option value='sortOption'>sort by...</option>
-                    </select>
-                    <input type='search' placeholder='search' />
-                </form>
-            </nav>
         )
     }
 }
