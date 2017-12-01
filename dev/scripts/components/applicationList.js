e445f3f3fcbc30e1eadca2d6de734c94da012cef
Nikolai Howley <nikolai.howley@gmail.com>
2017-12-01 12:48:01 -0500

    nailed sorting filters

--- dev/scripts/components/applicationList.js
+++ dev/scripts/components/applicationList.js
@@ -7,35 +7,49 @@ import moment from 'moment';
 export default class ApplicationList extends React.Component {
     constructor() {
         super();
+        this.state = {
+            filteredApplications: [],
+            filtered: false
+        }
         this.filterApplications = this.filterApplications.bind(this);
         this.handleChange = this.handleChange.bind(this);
     }
 
     handleChange(e) {
-        console.log(e.target.value);
         this.filterApplications(e.target.value);
     }
 
     filterApplications(days) {
-        const applications = this.props.applications;
-        const presentDate = moment();
-        // console.log(presentDate);
+        let filteredApplications = this.props.applications;
+        if(days != 'allApplications'){
+            const applications = this.props.applications;
+            const presentDate = moment();
+            // console.log(presentDate);
 
-        const compareDate = moment().subtract(days, 'days');
-        // console.log(compareDate);
-        
-        const filteredApplications = applications.filter((application) => {
-            const appliedMoment = moment(application.dateApplied, "YYYY-MM-DD");
-            const betweenBoolean = appliedMoment.isBetween(compareDate, presentDate);
-            console.log(betweenBoolean);
-            if(betweenBoolean){
-                return application;                
-            }
-        });
-        console.log(filteredApplications)
-        // moment('2010-10-20').isBetween('2010-10-19', '2010-10-25');
+            const compareDate = moment().subtract(days, 'days');
+            // console.log(compareDate);
+
+            filteredApplications = applications.filter((application) => {
+                const appliedMoment = moment(application.dateApplied, "YYYY-MM-DD");
+                const betweenBoolean = appliedMoment.isBetween(compareDate, presentDate);
+                console.log(betweenBoolean);
+                if (betweenBoolean) {
+                    return application;
+                }
+            });
+        }
+        this.setState({
+            filtered: true,
+            filteredApplications
+        })
     }   
 
+    componentDidMount() {
+        const newState = Array.from(this.props.applications)
+        this.setState({
+            filteredApplications: newState
+        })
+    }
     render() {
         return (
             <div>
@@ -48,7 +62,8 @@ export default class ApplicationList extends React.Component {
                     </select>
                 </nav>
                 <ul>
-                    {this.props.applications.map((item) => {
+                    {this.state.filtered
+                    ? this.state.filteredApplications.map((item) => {
                         return (
                             <Link to={`/application/${item.key}`} key={item.key}>
                                 <h3>{item.company}</h3>
@@ -56,7 +71,16 @@ export default class ApplicationList extends React.Component {
                                 <p>Last changed...</p>
                             </Link>
                         )
-                    })}
+                    }) : this.props.applications.map((item) => {
+                            return (
+                                <Link to={`/application/${item.key}`} key={item.key}>
+                                    <h3>{item.company}</h3>
+                                    <h4>{item.title}</h4>
+                                    <p>Last changed...</p>
+                                </Link>
+                            )
+                        })
+                    } 
                 </ul>
             </div>
         )
