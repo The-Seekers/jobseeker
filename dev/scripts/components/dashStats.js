7433967833ebc52cbdaa94d0953652215c380f42
Dylan On <dylanon@users.noreply.github.com>
2017-12-02 16:02:04 -0500

    Count upcoming interviews, display number in stats

--- dev/scripts/components/dashStats.js
+++ dev/scripts/components/dashStats.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import moment from 'moment';
 
 // at-a-glance section
 export default class DashStats extends React.Component {
@@ -10,12 +11,21 @@ export default class DashStats extends React.Component {
         } else if (isFiltered === true) {
             totalJobs = this.props.sorted.filteredApplications.length;
         }
+        
+        // Count upcoming interviews
+        const applications = this.props.applications.applications;
+        const interviewsArray = applications.filter((application) => {
+            // Return applications with an interview date of today or in the future
+            return application.interview && moment(application.interview, 'YYYY-MM-DD').isSameOrAfter(moment(), 'day');
+        });
+        const totalInterviews = interviewsArray.length;
+
         return (
             <section>
                 <ul>
                     <li>viewing {totalJobs} applications.</li>
                     <li></li>
-                    <li>temp statcard</li>
+                    <li>{totalInterviews} upcoming interviews</li>
                 </ul>
             </section>
         )
