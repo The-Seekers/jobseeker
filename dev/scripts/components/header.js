027a7039b2937c657a527309a052183482dbf330
Sam Janosik <samanthajanosik@gmail.com>
2017-12-04 14:47:41 -0500

    header styling and mobile nav

--- dev/scripts/components/header.js
+++ dev/scripts/components/header.js
@@ -15,25 +15,64 @@ export default class MainHeader extends React.Component {
         });
     }
 
+    navToggle(){
+        let links = document.querySelector('.narrowControls');
+        // if (links.style.display === 'block') {
+        //     links.style.display = 'none';
+        // } else {
+        //     links.style.display = 'block';
+        // }
+        if (links.style.top === '60px') {
+            links.style.top = '-200px';
+        } else {
+            links.style.top = '60px';
+        }
+    }
+
     render() {
         const shareUrl = `${window.location.origin}/shared/${this.props.userId}/${this.props.shareKey}`;
         return (
-            <header>
-                <h1>jobseekers anon.</h1>
-                {this.props.isLoggedIn &&
-                    // Only display sharing toggle if user is logged in
-                    <SharingToggle shareApplications={this.props.shareApplications} toggleSharing={this.props.toggleSharing} />
-                }
+            <header className="clearfix">
+                <div className="mainHeading clearfix">
+                    <img src="./public/assets/logo.svg" alt="job seekers logo" />
+                    <h1>job<span>seeker</span></h1> 
+                </div>
+                <div className="navWide">
+                    <nav className="wideControls">
+                        {this.props.isLoggedIn &&
+                            // Only display sharing toggle if user is logged in
+                            <SharingToggle shareApplications={this.props.shareApplications} toggleSharing={this.props.toggleSharing} />
+                        }
 
-                {this.props.isLoggedIn && this.props.shareApplications &&
-                    // Only display sharing link if user is logged in & sharing is enabled
-                    <p>Your public sharing link (anyone with the link can view): <a href={shareUrl}>{shareUrl}</a></p>
-                }
+                        {this.props.isLoggedIn && this.props.shareApplications &&
+                            // Only display sharing link if user is logged in & sharing is enabled
+                            <p>Your public sharing link (anyone with the link can view): <a href={shareUrl}>{shareUrl}</a></p>
+                        }
 
-                {this.props.isLoggedIn &&
-                    <p><a href="#" onClick={this.signOut}>sign out</a></p>
-                }
+                        {this.props.isLoggedIn &&
+                            <p><a href="#" onClick={this.signOut}>sign out</a></p>
+                        }
+                    </nav>
+                </div>
+                <div className="navNarrow">
+                    <i class="fa fa-bars" onClick={this.navToggle}></i>
+                    <nav className="narrowControls">
+                        {this.props.isLoggedIn &&
+                            // Only display sharing toggle if user is logged in
+                            <SharingToggle shareApplications={this.props.shareApplications} toggleSharing={this.props.toggleSharing} />
+                        }
 
+                        {this.props.isLoggedIn && this.props.shareApplications &&
+                            // Only display sharing link if user is logged in & sharing is enabled
+                            <p>Your public sharing link (anyone with the link can view): <a href={shareUrl}>{shareUrl}</a></p>
+                        }
+
+                        {this.props.isLoggedIn &&
+                            <p><a href="#" onClick={this.signOut}>sign out</a></p>
+                        }
+                    </nav>
+
+                </div>
             </header>
         )
     }
