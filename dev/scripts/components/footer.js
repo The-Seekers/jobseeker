5497fe0636548810e7cf1af98d9b99620ba4262f
Sam Janosik <samanthajanosik@gmail.com>
2017-12-04 16:36:38 -0500

    fixed colour conflicts and footer

--- dev/scripts/components/footer.js
+++ dev/scripts/components/footer.js
@@ -7,10 +7,6 @@ export default class MainFooter extends React.Component {
         return (
             <footer>
                 <p>With you until you're <span>hired</span></p>
-                <div className="logo clearfix">
-                    <img src="./public/assets/logo.svg" alt="job seekers logo" />
-                    <h1>job<span>seeker</span></h1>
-                </div>
             </footer>
         )
     }
