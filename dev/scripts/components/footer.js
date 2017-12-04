d27f854d2c78fdade64592c361d221e7ea39a45f
Sam Janosik <samanthajanosik@gmail.com>
2017-12-04 16:23:26 -0500

    working with header and footer

--- dev/scripts/components/footer.js
+++ dev/scripts/components/footer.js
@@ -7,6 +7,10 @@ export default class MainFooter extends React.Component {
         return (
             <footer>
                 <p>With you until you're <span>hired</span></p>
+                <div className="logo clearfix">
+                    <img src="./public/assets/logo.svg" alt="job seekers logo" />
+                    <h1>job<span>seeker</span></h1>
+                </div>
             </footer>
         )
     }
