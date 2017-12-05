4bdd64134d9981fd0db13b50db95f10ed93e57e1
Sam Janosik <samanthajanosik@gmail.com>
2017-12-05 17:40:52 -0500

    added some wrappers

--- dev/scripts/components/header.js
+++ dev/scripts/components/header.js
@@ -49,58 +49,60 @@ export default class MainHeader extends React.Component {
     render() {
         const shareUrl = `${window.location.origin}/shared/${this.props.userId}/${this.props.shareKey}`;
         return (
-            <header className="clearfix">
-                <div className="logo clearfix">
-                    <img src="/public/assets/logo.svg" alt="job seekers logo" />
-                    <h1>job<span>seeker</span></h1> 
-                </div>
-                <div className="navWide">
-                    <nav className="wideControls clearfix">
-                        {this.props.isLoggedIn &&
-                            // Only display sharing toggle if user is logged in
-                            <SharingToggle shareApplications={this.props.shareApplications} toggleSharing={this.props.toggleSharing} />
-                        }
-
-                        {this.props.isLoggedIn && this.props.shareApplications &&
-                            // Only display sharing link if user is logged in & sharing is enabled
-                                <button className="showShareLink" onClick={this.showShare}>Share Link</button>
-                        }
-
-                        {this.props.isLoggedIn &&
-                            <button onClick={this.signOut} className="signOut">Sign Out</button>
-                        }
-                    </nav>
-                </div>
-                <div className="navNarrow clearfix">
-                    {this.props.isLoggedIn &&
-                        <i className="fa fa-bars" onClick={this.navToggle}></i>
-                    }
-                    <nav className="narrowControls">
-                        {this.props.isLoggedIn &&
-                            // Only display sharing toggle if user is logged in
-                            <SharingToggle shareApplications={this.props.shareApplications} toggleSharing={this.props.toggleSharing} />
-                        }
-
-                        {this.props.isLoggedIn && this.props.shareApplications &&
-                            // Only display sharing link if user is logged in & sharing is enabled
-                            <button className="showShareLink" onClick={this.showShare}>Share Link</button>
-                        }
-
-                        {this.props.isLoggedIn &&
-                            <button onClick={this.signOut} className="signOut">Sign Out</button>
-                        }
-                    </nav>
-
-                </div>
-                {this.props.isLoggedIn && this.props.shareApplications && this.state.showShare &&
-                    <div className="shareOverlay">
-                        <div className="shareLink">
-                            <i className="fa fa-times" onClick={this.closeLink}></i>
-                            <p>This is your public sharing read-only link. Everytime sharing is turned on, a new unique link is generated. When sharing is turned off, the previous link is no longer valid. Copy and paste this link into an email to share your progress with anyone you want!</p>
-                            <a href={shareUrl} target="blank">{shareUrl}</a>
-                        </div>
+            <header>
+                <div className="wrapper clearfix">
+                    <div className="logo clearfix">
+                        <img src="/public/assets/logo.svg" alt="job seekers logo" />
+                        <h1>job<span>seeker</span></h1> 
                     </div>
-                }
+                    <div className="navWide">
+                        <nav className="wideControls clearfix">
+                            {this.props.isLoggedIn &&
+                                // Only display sharing toggle if user is logged in
+                                <SharingToggle shareApplications={this.props.shareApplications} toggleSharing={this.props.toggleSharing} />
+                            }
+
+                            {this.props.isLoggedIn && this.props.shareApplications &&
+                                // Only display sharing link if user is logged in & sharing is enabled
+                                <button className="showShareLink" onClick={this.showShare}>Share Link</button>
+                            }
+
+                            {this.props.isLoggedIn &&
+                                <button onClick={this.signOut} className="signOut">Sign Out</button>
+                            }
+                        </nav>
+                    </div>
+                    <div className="navNarrow clearfix">
+                        {this.props.isLoggedIn &&
+                            <i className="fa fa-bars" onClick={this.navToggle}></i>
+                        }
+                        <nav className="narrowControls">
+                            {this.props.isLoggedIn &&
+                                // Only display sharing toggle if user is logged in
+                                <SharingToggle shareApplications={this.props.shareApplications} toggleSharing={this.props.toggleSharing} />
+                            }
+
+                            {this.props.isLoggedIn && this.props.shareApplications &&
+                                // Only display sharing link if user is logged in & sharing is enabled
+                                <button className="showShareLink" onClick={this.showShare}>Share Link</button>
+                            }
+
+                            {this.props.isLoggedIn &&
+                                <button onClick={this.signOut} className="signOut">Sign Out</button>
+                            }
+                        </nav>
+
+                    </div>
+                    {this.props.isLoggedIn && this.props.shareApplications && this.state.showShare &&
+                        <div className="shareOverlay">
+                            <div className="shareLink">
+                                <i className="fa fa-times" onClick={this.closeLink}></i>
+                                <p>This is your public sharing read-only link. Everytime sharing is turned on, a new unique link is generated. When sharing is turned off, the previous link is no longer valid. Copy and paste this link into an email to share your progress with anyone you want!</p>
+                                <a href={shareUrl} target="blank">{shareUrl}</a>
+                            </div>
+                        </div>
+                    }
+                </div>
             </header>
         )
     }
